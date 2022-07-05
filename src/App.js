a4e178cb52e40a56646db96356f4dbccd0471b2a
Ali Shanawar <alishanawar409@gmail.com>
2022-07-05 16:54:02 +0500

    ~hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import Resturant from './components/Basics/Resturant';
+// import Resturant from './components/Basics/Resturant';
+// import UseState from './components/Hooks/useState';
+// import UseEffect from './components/Hooks/useEffect';
+import UseReducer from './components/Hooks/useReducer';
 
 const App = () => {
   return (
-    <Resturant/>
+    <UseReducer/>
+    // <UseState/>
+    // <Resturant/>
   )
 }
 
